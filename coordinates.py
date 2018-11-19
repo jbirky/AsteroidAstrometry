9d4450f8ef983d0d528f16ab39a6df4714826363
jbirky <jbirky@ucsd.edu>
2018-11-19 13:06:20 -0800

    format ra/dec to degrees

--- coordinates.py
+++ coordinates.py
@@ -2,6 +2,17 @@ import numpy as np
 from numpy import cos, sin
 
 
+def formatRADEC(ras, des):
+    """
+    Format RA/DEC from strings to degrees
+    """
+    ra0 = 15*(float(ras[0:2]) + float(ras[3:5])/60. + float(ras[6:])/3600.)
+    dsgn = np.sign(float(des[0:3]))
+    dec0 = float(des[0:3]) + dsgn*float(des[4:6])/60. + dsgn*float(des[7:])/3600.
+    
+    return ra0, dec0
+
+
 def angleToProjected(**kwargs):
     ra0 = kwargs.get('ra0')
     dec0 = kwargs.get('dec0')
