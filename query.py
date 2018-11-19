19337f172388fdc2751e9e43dd93ca7c87ec5b4c
jbirky <jbirky@ucsd.edu>
2018-11-19 11:49:09 -0800

    fix datatype errors

--- query.py
+++ query.py
@@ -1,6 +1,5 @@
-import urllib.request 
+import urllib
 import numpy as np
-import string as str
 import pdb
 
 def usno(radeg,decdeg,fovam,epoch):
@@ -15,8 +14,12 @@ def usno(radeg,decdeg,fovam,epoch):
     weburl = str1+str2
     
     print('Calling Vizier', weburl)
-    with urllib.request.urlopen(weburl) as url:
-        s = url.read()
+    try: # Python 2
+        with urllib.urlopen(weburl) as url:
+            s = url.read()
+    except: # Python 3
+        with urllib.request.urlopen(weburl) as url:
+            s = url.read()
     sl = s.splitlines()
     sl = sl[45:-1] # get rid of header - updated Oct 2013
     
@@ -26,16 +29,18 @@ def usno(radeg,decdeg,fovam,epoch):
     rmag = np.array([]) # rmage
     
     for k in sl:
-        kw = k.decode('UTF-8').split('\t')
+        try:
+            kw = k.split('\t')
+        except:
+            kw = k.decode('UTF-8').split('\t')
         ded0 = float(kw[2])
         pmrad = float(kw[6])/3600e3/np.cos(np.deg2rad(ded0)) # comvert from mas/yr to deg/year
         pmded = float(kw[7])/3600e3
         name = np.append(name,kw[0])
         rad = np.append(rad,float(kw[1]) + pmrad*(epoch-2000.0))
         ded = np.append(ded,float(kw[2]) + pmded*(epoch-2000.0))
-        print(kw[12])
-        if kw[12] != ' ': # case when no mag is reported
+        try: 
             rmag = np.append(rmag,float(kw[12]))
-        else:
+        except:
             rmag = np.append(rmag,np.nan)
     return name,rad,ded,rmag
