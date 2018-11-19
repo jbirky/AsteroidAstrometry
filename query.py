ffd39b05a8065138e8ac53245ce582893ee62027
jbirky <jbirky@ucsd.edu>
2018-11-18 23:32:35 -0800

    python 3 fixes

--- query.py
+++ query.py
@@ -14,7 +14,7 @@ def usno(radeg,decdeg,fovam,epoch):
     str2 = '&-c.ra={:4.6f}&-c.dec={:4.6f}&-c.bm={:4.7f}/{:4.7f}&-out.max=unlimited'.format(radeg,decdeg,fovam,fovam)
     weburl = str1+str2
     
-    print('Calling Vizier',str)
+    print('Calling Vizier', weburl)
     with urllib.request.urlopen(weburl) as url:
         s = url.read()
     sl = s.splitlines()
@@ -24,14 +24,16 @@ def usno(radeg,decdeg,fovam,epoch):
     rad = np.array([]) # RA in degrees
     ded = np.array([]) # DEC in degrees
     rmag = np.array([]) # rmage
+    
     for k in sl:
-        kw = k.split('\t')
+        kw = k.decode('UTF-8').split('\t')
         ded0 = float(kw[2])
         pmrad = float(kw[6])/3600e3/np.cos(np.deg2rad(ded0)) # comvert from mas/yr to deg/year
         pmded = float(kw[7])/3600e3
         name = np.append(name,kw[0])
         rad = np.append(rad,float(kw[1]) + pmrad*(epoch-2000.0))
         ded = np.append(ded,float(kw[2]) + pmded*(epoch-2000.0))
+        print(kw[12])
         if kw[12] != ' ': # case when no mag is reported
             rmag = np.append(rmag,float(kw[12]))
         else:
