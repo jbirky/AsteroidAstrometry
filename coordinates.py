4522e7df2115702c456252ded3c8d457e97a385a
jbirky <jbirky@ucsd.edu>
2018-12-04 15:34:25 -0800

    fix conversions

--- coordinates.py
+++ coordinates.py
@@ -16,32 +16,39 @@ def formatRADEC(ras, des):
 
 
 def angleToProjected(**kwargs):
-	"""
-	Transform RA/DEC coordinates (degrees) into projected sky coordinates
-	"""
-	ra0  = kwargs.get('ra0')
-	dec0 = kwargs.get('dec0')
-	ra   = kwargs.get('ra')
-	dec  = kwargs.get('dec')
-	fovam = kwargs.get('fovam', 4.0)
+    """
+    Transform RA/DEC coordinates (degrees) into projected sky coordinates
+    """
+    ra0  = kwargs.get('ra0')
+    dec0 = kwargs.get('dec0')
+    ra   = kwargs.get('ra')
+    dec  = kwargs.get('dec')
+    fovam = kwargs.get('fovam', 4.0)
 
-	X = cos(dec)*sin(ra-ra0) / (cos(dec0)*cos(dec)*cos(ra-ra0) + sin(dec)*sin(dec0))
-	Y = sin(dec0)*cos(dec)*cos(ra-ra0) - sin(dec)*cos(dec0) / (cos(dec0)*cos(dec)*cos(ra-ra0) + sin(dec)*sin(dec0))
-	#convert to radians
-	X, Y = -X*math.pi/180, -Y*math.pi/180
+    #convert degrees to radians
+    ra0  = ra0*math.pi/180
+    dec0 = dec0*math.pi/180
+    ra   = ra*math.pi/180
+    dec  = dec*math.pi/180
 
-	if kwargs.get('plot', True) == True:
-		plt.figure(figsize=[8,8])
-		plt.scatter(X, Y, color='k', edgecolor='none')
-		scale = fovam/2/60*math.pi/180 #convert arcmin -> arcsec -> radian
-		plt.xlim(-scale, scale)
-		plt.ylim(-scale, scale)
-		plt.xlabel('X')
-		plt.ylabel('Y')
-		plt.title('USNO-B1.0 Projected Coordinates')
-		plt.show()
 
-	return X, Y
+    X = cos(dec)*sin(ra-ra0) / (cos(dec0)*cos(dec)*cos(ra-ra0) + sin(dec)*sin(dec0))
+    Y = sin(dec0)*cos(dec)*cos(ra-ra0) - sin(dec)*cos(dec0) / (cos(dec0)*cos(dec)*cos(ra-ra0) + sin(dec)*sin(dec0))
+    #convert to radians
+    X, Y = -X, -Y
+
+    if kwargs.get('plot', True) == True:
+        plt.figure(figsize=[8,8])
+        plt.scatter(X, Y, color='k', edgecolor='none')
+        scale = fovam/2/60*math.pi/180 #convert arcmin -> arcsec -> radian
+        plt.xlim(-scale, scale)
+        plt.ylim(-scale, scale)
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.title('USNO-B1.0 Projected Coordinates')
+        plt.show()
+
+    return X, Y
 
 
 def projectedToPixelIdeal(Xproj, Yproj, **kwargs):
@@ -57,18 +64,36 @@ def projectedToPixelIdeal(Xproj, Yproj, **kwargs):
     return x, y
 
 
-def projectedToPixel(xccd, yccd, Xproj, Yproj, **kwargs):
+def projectedToPixelIdealInv(Xccd, Yccd, **kwargs):
+    """
+    Apply ideal coordinate transformation.
+    """
+    f_p = kwargs.get('f_p', 190020)
+    
+    Xccd, Yccd = np.array(Xccd), np.array(Yccd)
+    x0 = y0 = 512
+    
+    Xproj = (Xccd - x0)/f_p
+    Yproj = (Yccd - y0)/f_p
+
+    return Xproj, Yproj
+
+
+def projectedToPixel(xccd, yccd, Xusno, Yusno, **kwargs):
     """
     Apply shear, scale and rotation coordinate transformation.
     """
+    xccd, yccd   = np.array(xccd), np.array(yccd)
+    Xusno, Yusno = np.array(Xusno), np.array(Yusno)
+    
     f_p = kwargs.get('f_p', 190020)
-    if len(xccd) != len(Xproj):
-        print('xccd and Xproj are not the same dimension!')
+    if len(xccd) != len(Xusno):
+        print('xccd and Xusno are not the same dimension!')
         return
     else:
         N = len(xccd)
     
-    B = np.array([f_p*Xproj, f_p*Yproj, np.ones(N)]).T
+    B = np.array([f_p*Xusno, f_p*Yusno, np.ones(N)]).T
     c = np.dot(np.linalg.inv(np.dot(B.T, B)), B.T)
     
     ax = np.dot(B, np.dot(c, xccd))
@@ -77,7 +102,11 @@ def projectedToPixel(xccd, yccd, Xproj, Yproj, **kwargs):
     T = np.array([[f_p*ax[0], f_p*ax[1], ax[2]],
                   [f_p*ay[0], f_p*ay[1], ay[2]],
                   [0,         0,         1    ]])
-    XY = np.array([Xproj, Yproj, np.ones(N)]).T
-    xy = np.dot(T, XY)
+
+    xy = np.array([xccd, yccd, np.ones(N)])
+    Tinv = np.linalg.inv(T)
     
-    return x, y
+    Xccd = np.dot(Tinv, xy)[0]
+    Yccd = np.dot(Tinv, xy)[1]
+    
+    return Xccd, Yccd
